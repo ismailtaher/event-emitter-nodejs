be7ae635e910a857cb1e980b9b94327245d01a89
Ismail Taher <ismail.taher@ymail.com>
2025-07-23 04:13:33 +0500

    add index.js and emit event inside it, events logged to logs>eventLog.txt

--- logEvents.js
+++ logEvents.js
@@ -8,10 +8,17 @@ const path = require("path");
 
 const logEvents = async (message) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
-  const logItem = `${dateTime}\t${uuid()}\t${message}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message} \n`;
   console.log(logItem);
   try {
-    await fsPromises.appendFile(path.join(__dirname, "logs", "eventLog.txt"));
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+      //testing
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", "eventLog.txt"),
+      logItem
+    );
   } catch (err) {
     console.log(err);
   }
